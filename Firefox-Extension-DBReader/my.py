097b294aaf6894350e583b3f6a73885256eedde1
happmaoo <happmaoo@gmail.com>
2023-09-14 09:31:12 +0800

    Add files via upload

---
+++ Firefox-Extension-DBReader/my.py
@@ -0,0 +1,39 @@
+
+import argparse
+import collections.abc
+import json
+import pathlib
+from pprint import pprint
+import re
+import os
+import sys
+
+import mozidb
+
+
+"""
+		说明，需要安装：
+		pip install python-snappy
+		
+"""
+
+
+db_data=[]
+
+db = mozidb.IndexedDB("my.sqlite")
+db_data = db.read_objects()
+
+#print(db_data)
+
+
+data = []
+
+
+for dict_key in db_data.keys():
+	print(dict_key)
+
+for dict_value in db_data.values():
+	print(dict_value)
+
+
+
